5590d7ba76a45d3f50e808807afeb38381dce0f8
Daan van Renterghem <dvrenterghem@ibuildings.nl>
2013-08-27 16:02:50 +0200

    First pass at setting up saml-auth

---
+++ src/SURFnet/OneLoginBridgeBundle/Controller/SamlController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace SURFnet\OneLoginBridgeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class DefaultController
+ * @package SURFnet\OneLoginBridgeBundle\Controller
+ *
+ * @Route("/saml")
+ *
+ * @author Daan van Renterghem <dvrenterghem@gmail.com>
+ */
+class SamlController extends Controller
+{
+    /**
+     * @Route("/login")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function loginAction()
+    {
+        return $this->render('SURFnetOneLoginBridgeBundle:Default:index.html.twig', array('name' => 'Daan'));
+    }
+
+    /**
+     * @Route("/metadata")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function metadataAction()
+    {
+        return $this->render('SURFnetOneLoginBridgeBundle:Default:index.html.twig', array('name' => 'Daan'));
+    }
+
+    /**
+     * @Route("/consumer")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function consumerAction()
+    {
+        return $this->render('SURFnetOneLoginBridgeBundle:Default:index.html.twig', array('name' => 'Daan'));
+    }
+}
