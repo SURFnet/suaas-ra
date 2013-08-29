73d560cd61ed7753fb3df8372be575cdd7866ee5
Daan van Renterghem <dvrenterghem@ibuildings.nl>
2013-08-29 13:32:25 +0200

    Expand domain functionality to convert saml data to a SAMLIdentity
    - this identity can later be used to create/retrieve users
      which leads to the determining of what the next step of
      the user is.

---
+++ src/SURFnet/SuAAS/DomainBundle/Service/SAML/IdentityResolver.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SURFnet\SuAAS\DomainBundle\Service\SAML;
+
+use SURFnet\OneLoginBridgeBundle\SAML\Attribute\DisplayName;
+use SURFnet\OneLoginBridgeBundle\SAML\Attribute\Mail;
+use SURFnet\OneLoginBridgeBundle\SAML\Attribute\SchacHomeOrganisation;
+use SURFnet\OneLoginBridgeBundle\SAML\Attribute\Uid;
+use SURFnet\OneLoginBridgeBundle\Service\ResponseAdapter;
+use SURFnet\SuAAS\DomainBundle\Entity\SAMLIdentity;
+
+class IdentityResolver
+{
+    public function parse(ResponseAdapter $response)
+    {
+        return new SAMLIdentity(
+            $response->getNameId(),
+            $response->getAttribute(Uid::NAME),
+            $response->getAttribute(SchacHomeOrganisation::NAME),
+            $response->getAttribute(DisplayName::NAME),
+            $response->getAttribute(Mail::NAME)
+        );
+    }
+}
