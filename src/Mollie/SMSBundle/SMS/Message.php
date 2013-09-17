11166134e01167994cc178f573d75f391f5954fb
Daan van Renterghem <dvrenterghem@ibuildings.nl>
2013-09-17 17:38:00 +0200

    Mollie\SMSBundle cleanup and documentation

--- src/Mollie/SMSBundle/SMS/Message.php
+++ src/Mollie/SMSBundle/SMS/Message.php
@@ -2,9 +2,24 @@
 
 namespace Mollie\SMSBundle\SMS;
 
+/**
+ * Class Message
+ * @package Mollie\SMSBundle\SMS
+ *
+ * DTO that contains the required dynamic fields to send a text-message
+ *
+ * @author Daan van Renterghem <dvrenterghem@ibuildings.nl>
+ */
 class Message
 {
+    /**
+     * @var string the body of the message. May not be longer than 160 characters
+     */
     public $body;
+
+    /**
+     * @var string the 8 character phone-number of the recipient (Everything after 06-)
+     */
     public $recipient;
 }
 
--- src/Mollie/SMSBundle/SMS/Service.php
+++ src/Mollie/SMSBundle/SMS/Service.php
@@ -4,15 +4,51 @@ namespace Mollie\SMSBundle\SMS;
 
 use Guzzle\Http\Client;
 
+/**
+ * Class Service
+ * @package Mollie\SMSBundle\SMS
+ *
+ * Service object that allows the sending of text-messages via Mollie SMS
+ *
+ * @author Daan van Renterghem <dvrenterghem@ibuildings.nl>
+ */
 class Service
 {
+    /**
+     * Mollie URL to send texts via SMS. Choose to have const over config, since
+     * this will never change - not unless they're willing to force all their
+     * clients to update this.
+     */
     CONST BASE_URL = "https://api.messagebird.com/xml/sms/";
 
+    /**
+     * @var string username to authenticate with at Mollie SMS
+     */
     private $username;
+
+    /**
+     * @var string password to authenticate with at Mollie SMS
+     */
     private $password;
+
+    /**
+     * @var string name (11 char max) displayed as sender
+     */
     private $originator;
+
+    /**
+     * @var Client
+     */
     private $client;
 
+    /**
+     * Constructor.
+     *
+     * @param Client $client
+     * @param string $username
+     * @param string $password
+     * @param string $originator
+     */
     public function __construct(Client $client, $username, $password, $originator)
     {
         $this->client = $client;
@@ -21,6 +57,12 @@ class Service
         $this->originator = $originator;
     }
 
+    /**
+     * Send a text via SMS
+     *
+     * @param Message $message
+     * @return bool indicates whether or not the message has successfully been sent
+     */
     public function send(Message $message)
     {
         $request = $this->client->get(self::BASE_URL);
@@ -37,7 +79,7 @@ class Service
 
         $xml = $response->xml();
 
-        // handling should be improved 10-fold
+        // handling should be improved 10-fold in production
         // see https://www.mollie.nl/support/documentatie/sms-diensten/sms/http/
         if (reset($xml->item->success) !== 'true') {
             return false;
